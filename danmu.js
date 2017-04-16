4ed8a439a9655e49ccf5bca822e9fece542d40a4
steven <1099326846@qq.com>
2017-04-16 10:29:53 +0800

    Add files via upload

---
+++ danmu.js
@@ -0,0 +1,65 @@
+﻿var ref;
+var arrContext = [];
+var arrData = [];
+
+function createSync(){
+	var config = {
+		authDomain:"dmapp1.wilddogio.com",
+		syncURL: "https://dmapp1.wilddogio.com/message"};
+	wilddog.initializeApp(config);
+	ref = wilddog.sync().ref();
+}
+
+function subContext(){
+	var context = $(".danmu-input-box").val();
+	arrContext.push(context);
+	ref.set(arrContext);
+	$(".danmu-input-box").val("");
+}
+
+function clsContext(){
+	ref.remove();
+	arrContext = [];
+	arrData = [];
+	$(".danmu-screen").empty();
+}
+
+function listenData(){
+	var data;
+	ref.on("child_added",function(snapshot){
+		data = snapshot.val();
+		arrData.push(data);
+	});	
+}
+
+function run(){
+	var num;
+	var ranColor;
+	for(var i=0;i<arrData.length;i++){
+		num = Math.floor(Math.random()*341+9).toString()+"px";
+		ranColor = contextColor();
+		$(".danmu-screen").append('<div id="d'+i+'">'+arrData[i]+'</div>');	
+		$('#d'+i).css({"position":"absolute","right":"0","top":num,"color":ranColor});
+		$('#d'+i).animate({left:"0"},3000,function(){
+			$(".danmu-screen").empty();
+		});
+	}
+	setTimeout(run,5000);
+}
+
+function contextColor(){
+	var red = Math.floor(Math.random()*255).toString();
+	var green = Math.floor(Math.random()*255).toString();
+	var blue = Math.floor(Math.random()*255).toString();	
+	var color = "rgb("+red+","+green+","+blue+")";
+	
+	return color;
+}
+
+$(document).ready(function(){
+	createSync();
+	listenData();
+	run();
+	$(".danmu-sub").on("click",subContext);
+	$(".danmu-cls").on("click",clsContext);
+});
